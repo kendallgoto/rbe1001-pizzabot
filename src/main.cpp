661252b470552a23df67bb5d39f5dc1ce106e113
suryanshg <suryanshg47@gmail.com>
2020-02-29 20:01:56 -0500

    Added autonomous_OED, front-wheels going over speed bump.

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,7 @@ bool clawClosed = false; // Variable to keep track of claw's state, true for clo
 void moveMotors(Motor motorArray[], int size, double position, double velocity, bool shouldBlock, bool isRelative, bool isTurning);
 void drive(double distance, bool forward);
 void turn(double angle);
+void autonomous_OED();
 
 /*
  * Main Function
@@ -85,7 +86,7 @@ void initialize() {
     intake_Positions[INTAKE_GROUND] = motor4Bar_1.get_position() - 5;
     intake_Positions[INTAKE_FLOOR2] = motor4Bar_1.get_position() + 245;
     intake_Positions[INTAKE_PIZZERIA] = motor4Bar_1.get_position() + 340;
-    intake_Positions[INTAKE_FLOOR3] = motor4Bar_1.get_position() + 355;
+    intake_Positions[INTAKE_FLOOR3] = motor4Bar_1.get_position() + 365;
     intake_Positions[INTAKE_FLOOR4] = motor4Bar_1.get_position() + 470;
     intake_Positions[INTAKE_FLOOR5] = motor4Bar_1.get_position() + 645;
 
@@ -120,7 +121,104 @@ void competition_initialize() {
 }
 
 /*
- * Function to operate on Autonomous Mode
+ * Function to operate on Autonomous Mode in OED
+ */
+void autonomous_OED(){
+    /* STRATEGY -- OED */
+    /* Start from Pizzeria, Check for starting side (Left/Right), make adjustments and Pick up Pizza */
+    /* Drive to Faraday */
+    /* Place Pizza on Floor 2 */
+    /* Drive back to Pizzeria */
+    /* Pick up Pizza */
+    /* Drive to Faraday */
+    /* Place Pizza on Floor 3 */
+    
+    //motorLeft.set_pos_pid_full();
+
+    BarState targetStates[2] = {INTAKE_FLOOR2, INTAKE_FLOOR3};
+    int currentState = 0;
+    for(int i = 0; i < 2; i++) {
+        // starting at 40 cm away from pizza slot
+        delay(500);
+        moveMotors(fourbar, 2, intake_Positions[INTAKE_PIZZERIA], 80, true, false, false); // set 4 bar to Pizzeria slot
+        delay(1000);
+        motorClaw.move_velocity(100); // Grab pizza
+        delay(1000);
+        moveMotors(fourbar, 2, intake_Positions[INTAKE_GROUND] + 80, 80, true, false, false); // set 4 bar to ground
+        /* Drive to Faraday */
+        delay(100);
+
+        drive(80, false);
+        delay(100);
+        turn(90);
+        delay(100);
+        drive(50, false);
+        delay(100);
+        drive(40, true);
+
+        /* Place Pizza on Floor i + 2 */
+        delay(100);
+        moveMotors(fourbar, 2, intake_Positions[targetStates[currentState++]], 80, true, false, false); // set 4 bar to Floor 2
+        delay(100);
+        drive(20, true);
+        delay(100);
+        motorClaw.move_absolute(clawOpenPos, 100); // open claw and deliver pizza
+
+        /* Drive back to Pizzeria */
+        delay(100);
+
+        drive(70, false);
+        delay(100);
+        moveMotors(fourbar, 2, intake_Positions[INTAKE_GROUND] + 80, 80, true, false, false); // set 4 bar to Ground 
+        delay(100);
+        drive(25, true);
+        delay(100);
+        turn(-100);
+        delay(100);
+        moveMotors(fourbar, 2, intake_Positions[INTAKE_PIZZERIA], 80, true, false, false); // set 4 bar to Pizzeria slot
+        delay(100);
+        drive(73, true);
+    }
+
+    moveMotors(fourbar, 2, intake_Positions[INTAKE_GROUND] + 80, 80, true, false, false); // set 4 bar to ground
+
+    /* Move to construction zone */
+    delay(100);
+    turn(45);
+    delay(100);
+    drive(15, true);
+    delay(100);
+    turn(45);
+    delay(100);
+    drive(40, false);
+    delay(100);
+    drive(180, true);
+    delay(100);
+    turn(90);
+    delay(1000);
+    drive(50, false);
+    delay(1000);
+    moveMotors(fourbar, 2, intake_Positions[INTAKE_FLOOR2], 80, true, false, false);
+    //plow full speed to get front wheel on
+    driveMotors[0].move(127);
+    driveMotors[1].move(127);
+    delay(3000); //We SHOULD be up by now.
+    driveMotors[0].move(0);
+    driveMotors[1].move(0);
+    //then, turn by backing up the left wheel
+    delay(250);
+    drive(20,false);
+    turn(-20);
+    delay(250);
+    driveMotors[0].move_velocity(100);
+    //driveMotors[1].move_velocity(70);
+    driveMotors[1].move_velocity(100);
+    delay(1500);
+
+}
+
+/*
+ * Function to operate on Autonomous Mode in CDR
  */
 void autonomous() {
     /* Strategy -- CDR */
@@ -170,44 +268,6 @@ void autonomous() {
         delay(100);
         drive(73, true);
     }
-
-    moveMotors(fourbar, 2, intake_Positions[INTAKE_GROUND] + 80, 80, true, false,
-                   false);
-    delay(100);
-    drive(5, false);
-    delay(100);
-    turn(90);
-    delay(100);
-    drive(40, false);
-    delay(100);
-    drive(180, true);
-    delay(100);
-    turn(90);
-    delay(1000);
-    drive(50, false);
-    delay(1000);
-    moveMotors(fourbar, 2, intake_Positions[INTAKE_PIZZERIA], 80, true, false, false);
-    //plow full speed to get front wheel on
-    moveMotors(driveMotors,2,1000,100, false,true, false);
-    delay(2000); //We SHOULD be up by now.
-    driveMotors[0].move(0);
-    driveMotors[1].move(0);
-    //then, turn by backing up the left wheel
-    turn(-15);
-    driveMotors[0].move_velocity(100);
-    driveMotors[1].move_velocity(70);
-    delay(800);
-    /* STRATEGY -- OED */
-    /* Start from Pizzeria, Check for starting side (Left/Right), make adjustments and Pick up Pizza */
-    /* Drive to Faraday */
-    /* Place Pizza on Floor 2 */
-    /* Drive back to Pizzeria */
-    /* Pick up Pizza */
-    /* Drive to Faraday */
-    /* Place Pizza on Floor 3 */
-    /* Move to construction zone */
-    /* Go over the speed bump */
-    //motorLeft.set_pos_pid_full();
 }
 
 /*
@@ -278,7 +338,7 @@ void opcontrol() {
                 delay(10);
             }
 
-            autonomous();
+            autonomous_OED();
         }
 
         /*
@@ -320,6 +380,10 @@ void opcontrol() {
             moveMotors(fourbar, 2, target, 80, false, false, false);
             intake_adjustment = target;
         }
+
+        /*
+         * Winch forward drive
+         */
         if(ctrl.get_digital(E_CONTROLLER_DIGITAL_LEFT)) {
             //set motor mode
             motor4Bar_1.set_brake_mode(E_MOTOR_BRAKE_COAST);
@@ -332,6 +396,10 @@ void opcontrol() {
             }
             motorWinch.move_velocity(0);
         }
+
+        /*
+         * Winch reverse drive
+         */
         if(ctrl.get_digital(E_CONTROLLER_DIGITAL_RIGHT)) {
             //set motor mode
             motorWinch.move_velocity(-100);
